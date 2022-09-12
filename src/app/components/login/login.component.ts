18c38527debc993a9d17ffa26ab69a6af33403ed
Dami <dami.odulaja@oaperg.com>
2022-09-12 19:09:48 +0100

    added Authentication Google Login & Logout

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {GoogleAuthProvider} from 'firebase/auth'
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
-  ngOnInit(): void {
+  login () {
+    this.afAuth.signInWithPopup(new GoogleAuthProvider()).then(() => console.log("Login Successful"))
   }
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Component, OnInit } from '@angular/core';
+import * as firebase  from 'firebase/compat';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent  {
+  user$: Observable<firebase.default.User | null> 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private afAuth: AngularFireAuth) { 
+    this.user$ = this.afAuth.authState
   }
 
+  logOut () {
+    this.afAuth.signOut().then(() => console.log("User signed out"))
+  }
 }
