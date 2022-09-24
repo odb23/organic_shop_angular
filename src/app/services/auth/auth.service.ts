f5e100321659fea37be029f200f26198bb03500a
Dami <dami.odulaja@oaperg.com>
2022-09-24 14:21:54 +0100

    added route guard to application

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,37 +1,52 @@
-import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { UserService } from './../user/user.service';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  Router,
+} from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { GoogleAuthProvider } from 'firebase/auth';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
+import { IAppUser } from 'src/app/models/app-user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private _user$:  Observable<firebase.default.User | null> 
+  private _user$: Observable<firebase.default.User | null>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private router: Router) {
-    this._user$ = this.afAuth.authState
+
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this._user$ = this.afAuth.authState;
   }
 
   logIn() {
-    let returnUrl = this.route.snapshot.queryParamMap.get("returnUrl") || "/"
-    localStorage.setItem("returnUrl", returnUrl)
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
 
-    this.afAuth
-      .signInWithPopup(new GoogleAuthProvider())
-      .then(() => {
-        console.log('Login Successful')
-        this.router.navigateByUrl(returnUrl);
-      });
-    
+    this.afAuth.signInWithPopup(new GoogleAuthProvider()).then(() => {
+      console.log('Login Successful');
+      this.router.navigateByUrl(returnUrl);
+    });
   }
 
   logOut() {
-     this.afAuth.signOut().then(() => console.log('User signed out'));
+    this.afAuth.signOut().then(() => console.log('User signed out'));
   }
 
-  get user () {
+  get user() {
     return this._user$;
+  }
+
+  get appUser$(): Observable<IAppUser | null> {
+    return this.user.pipe(
+      switchMap((user) => this.userService.get(user?.uid))
+    );
   }
 }
